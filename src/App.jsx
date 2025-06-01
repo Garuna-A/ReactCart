687068fc9d2b93a08f22f3a3ab5b76a685e9f615
Anurag mishra <146314079+Garuna-A@users.noreply.github.com>
2025-06-01 19:32:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
   import './App.css'
   import { Routes,Route } from 'react-router-dom'
   import Header from './components/Header'
-  import Home from './components/home'
+  import Home from './components/Home'
   import Cart from './components/Cart'
   function App() {
     const [cart,setCart] = useState([]);
