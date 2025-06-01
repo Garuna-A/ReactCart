47ed576601eeed52e706aba789bf4b5b4e6276e5
Garuna-A <mishra.anuragdli@gmail.com>
2025-06-01 15:39:42 +0530

    Increment item count on adding to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Home from './components/home'
 function App() {
+
+  const[cartItemCount,setCartItemCount] = useState(0);
+  const addtoCart = ()=>{
+    setCartItemCount(prev=>prev+1);
+  };
+
   return(
     <div>
-      <Header/>
-      <Home/>
+      <Header cartItemCount={cartItemCount}/>
+      <Home addtoCart={addtoCart}/>
     </div>
   )
 }
